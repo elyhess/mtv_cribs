c79cbd59bf42391a431cec0a414e3ccafe0d4536
Ely Hess <ely.hess@me.com>
2020-10-20 09:10:49 -0600

    Add apartment class

---
+++ test/apartment_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/renter'
+require_relative '../lib/apartment'
+
+
+class ApartmentTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+    assert_nil unit1.renter
+  end
+
+end
