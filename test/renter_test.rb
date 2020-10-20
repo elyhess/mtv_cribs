924d3a142f3de7b9424500d151eaa0d508b27ff6
Ely Hess <ely.hess@me.com>
2020-10-20 09:06:06 -0600

    Add renter class

---
+++ test/renter_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/renter'
+
+class RenterTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    renter1 = Renter.new("Jessie")
+
+    assert_equal "Jessie", renter1.name
+  end
+
+end
